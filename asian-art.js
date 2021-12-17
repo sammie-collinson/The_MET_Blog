c08cf51522400cd4d9dda7c6ae1a3ae3fef219a8
Sammie Collinson <heroldsamantha@gmail.com>
2021-12-17 15:44:42 -0500

    added 'scroll to top' button

--- asian-art.js
+++ asian-art.js
@@ -61,3 +61,21 @@ const getPhotoIds = async function (ids){
 for (i = 0; i < depts.length; i++) {
     getPhotoIds(depts[i]);
 }
+
+//Scroll to top button:
+const mybutton = document.getElementById("myBtn");
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "initial";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+window.onscroll = function() {scrollFunction()};
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
