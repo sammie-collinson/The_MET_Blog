626060c9b1985d01979b1d3c5ba1feb6de3ae6a9
Sammie Collinson <heroldsamantha@gmail.com>
2021-12-17 12:47:21 -0500

    connected added DOM elements to api for loops

--- photos.js
+++ photos.js
@@ -7,25 +7,38 @@ const getPhotoIds = async function (ids){
     
     const getObjects = async (ids) => {
         const response = await axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${ids}`);
-        imageArr.push(response.data.primaryImage);
-        console.log(imageArr);
+        imageArr.push(response.data.primaryImageSmall);
+        titleArr.push(response.data.title);
+        objDateArr.push(response.data.objectDate);
+        console.log(response.data);
 
         let newDiv = document.createElement('div');
         let headerTag = document.createElement('h4');
-        headerTag.classList.add('dept-text');
-        headerTag.innerText = 'test';
+        let dateTag = document.createElement('p');
+        headerTag.classList.add('img-desc');
+
+        for (i=0; i < titleArr.length; i++){
+            headerTag.innerText = titleArr[i];
+        }
+        for (i=0; i < titleArr.length; i++){
+            dateTag.innerText = `date: ${objDateArr[i]}`;
+        }
+
         let newImg = document.createElement('img');
-        newImg.src = imageArr[0];
+
+        for (i = 0; i < imageArr.length; i++) {
+            newImg.src = imageArr[i];
+        }
+
         newImg.classList.add('content');
         newDiv.appendChild(headerTag);
+        headerTag.appendChild(dateTag);
         newDiv.appendChild(newImg);
-        document.body.appendChild(newDiv);
-
-
-
-        
+        document.body.appendChild(newDiv);   
     }
     let imageArr = [];
+    let titleArr = [];
+    let objDateArr = [];
     for (i = 0; i < 10; i++) {
         getObjects(photoIds[i]);  
     }
